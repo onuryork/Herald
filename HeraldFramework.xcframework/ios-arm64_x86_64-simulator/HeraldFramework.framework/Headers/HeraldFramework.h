67805204657691a0d12027cf7160bd63946418d0
Onur Yörük <oyoeruek@cellular.de>
2020-12-28 23:44:35 +0800

    create demo spm

---
+++ HeraldFramework.xcframework/ios-arm64_x86_64-simulator/HeraldFramework.framework/Headers/HeraldFramework.h
@@ -0,0 +1,18 @@
+//
+//  HeraldFramework.h
+//  HeraldFramework
+//
+//  Created by Onur Yörük on 28.12.20.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for HeraldFramework.
+FOUNDATION_EXPORT double HeraldFrameworkVersionNumber;
+
+//! Project version string for HeraldFramework.
+FOUNDATION_EXPORT const unsigned char HeraldFrameworkVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <HeraldFramework/PublicHeader.h>
+
+
