d32445f362da48223fb57ab1a53a8d40c5eab8f3
Onur Yörük <oyoeruek@cellular.de>
2020-12-29 17:36:08 +0800

    rename framework to HeraldSDK

--- Package.swift
+++ Package.swift
@@ -19,6 +19,6 @@ let package = Package(
         // Targets are the basic building blocks of a package. A target can define a module or a test suite.
         // Targets can depend on other targets in this package, and on products in packages this package depends on.
         .binaryTarget(name: "HeraldSDK",
-                      path: "HeraldFramework.xcframework")
+                      path: "HeraldSDK.xcframework")
     ]
 )
